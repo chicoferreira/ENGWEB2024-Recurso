0184dc5983e7a26d90aa0c2f648d8b8a05c2e3dc
chico ferreira <36338391+chicoferreira@users.noreply.github.com>
2024-06-06 16:21:11 +0100

    ex2

--- ex2/routes/index.js
+++ ex2/routes/index.js
@@ -5,9 +5,28 @@ var axios = require('axios');
 const apiDados = "http://localhost:17000";
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
   axios.get(apiDados + "/books").then((response) => {
     res.render('index', { livros: response.data });
+  }).catch((error) => {
+    res.render('error', { error: error });
+  });
+});
+
+router.get('/authors/:idAutor', function (req, res) {
+  axios.get(apiDados + "/books").then((response) => {
+    let author = req.params.idAutor;
+    let livros = response.data;
+    let livrosAutor = livros.filter(livro => livro.author && livro.author.includes(author));
+    res.render('author', { author: author, livros: livrosAutor });
+  }).catch((error) => {
+    res.render('error', { error: error });
+  });
+});
+
+router.get('/:id', function (req, res) {
+  axios.get(apiDados + "/books/" + req.params.id).then((response) => {
+    res.render('livro', { livro: response.data });
   }).catch((error) => {
     res.render('error', { error: error });
   });
